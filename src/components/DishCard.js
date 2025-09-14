935d014f74efd988ee7ad6ba31b6a4277eefee3e
sudhakar076020 <sudhakar7602m@gmail.com>
2025-09-14 20:33:02 +0530

    if a user click the dish item the react popup will be open at the bottom of the page it consist of image, name, type, ingredient and add/remove btn, and also the user only click the dish card if the click the add/remove btn in the dishcard component each dish i doesn't open because using the event.stopPropagation() function to prevent the popup when clciking the add/remove btn

--- src/components/DishCard.js
+++ src/components/DishCard.js
@@ -1,4 +1,5 @@
 import "../App.css";
+import Popup from "reactjs-popup"; //Reactjs Popup model
 
 const DishCard = ({
   dish,
@@ -10,47 +11,104 @@ const DishCard = ({
   const isSelected = selectedDishes.some((item) => item.id === dish.id);
 
   return (
-    <div className="dish-card">
-      <div className="dish-details-content">
-        <div className="dish-name-type">
-          <h2>{dish.name}</h2>
-          <div
-            className={`dish-type ${
-              dish.type === "VEG" ? "veg-symbol" : "nonVeg-symbol"
-            }`}
-          >
-            <span
-              className={
-                dish.type === "VEG" ? "veg-dot-symbol" : "nonVeg-dot-symbol"
-              }
-            />
+    <Popup
+      trigger={
+        <div className="dish-card">
+          <div className="dish-details-content">
+            <div className="dish-name-type">
+              <h2>{dish.name}</h2>
+              <div
+                className={`dish-type ${
+                  dish.type === "VEG" ? "veg-symbol" : "nonVeg-symbol"
+                }`}
+              >
+                <span
+                  className={
+                    dish.type === "VEG" ? "veg-dot-symbol" : "nonVeg-dot-symbol"
+                  }
+                />
+              </div>
+            </div>
+            <p className="dish-description">{dish.description}</p>
+
+            <button
+              className="ingredient-link"
+              onClick={() => onViewIngredients(dish)}
+            >
+              <img
+                src="https://res.cloudinary.com/dehz5pshe/image/upload/v1757777589/fi_15315413_sopwdd.svg"
+                alt="ingrediant icon"
+              />
+              Ingredient
+            </button>
+          </div>
+
+          <div className="dish-img-content">
+            <img src={dish.image} alt={dish.name} />
+            <button
+              type="button"
+              className={isSelected ? "remove-btn" : "add-btn"}
+              onClick={(e) => {
+                e.stopPropagation(); //Prevent popup from opening
+                isSelected ? onRemoveDish(dish.id) : onAddDish(dish);
+              }}
+            >
+              {isSelected ? "Remove" : "Add +"}
+            </button>
           </div>
         </div>
-        <p className="dish-description">{dish.description}</p>
+      }
+      modal
+      nested
+    >
+      {() => (
+        <div className="popup-modal">
+          <div className="popup-content">
+            <img src={dish.image} alt={dish.name} />
+            <div className="dish-item">
+              <div className="dish-item-details">
+                <h2>{dish.name}</h2>
+                <div
+                  className={`dish-type ${
+                    dish.type === "VEG" ? "veg-symbol" : "nonVeg-symbol"
+                  }`}
+                >
+                  <span
+                    className={
+                      dish.type === "VEG"
+                        ? "veg-dot-symbol"
+                        : "nonVeg-dot-symbol"
+                    }
+                  />
+                </div>
+              </div>
+              <button
+                type="button"
+                className={isSelected ? "remove-btn" : "add-btn"}
+                onClick={() =>
+                  isSelected ? onRemoveDish(dish.id) : onAddDish(dish)
+                }
+              >
+                {isSelected ? "Remove" : "Add +"}
+              </button>
+            </div>
+            <p className="dish-description">{dish.description}</p>
 
-        <button
-          className="ingredient-link"
-          onClick={() => onViewIngredients(dish)}
-        >
-          <img
-            src="https://res.cloudinary.com/dehz5pshe/image/upload/v1757777589/fi_15315413_sopwdd.svg"
-            alt="ingrediant icon"
-          />
-          Ingredient
-        </button>
-      </div>
-
-      <div className="dish-img-content">
-        <img src={dish.image} alt={dish.name} />
-        <button
-          type="button"
-          className={isSelected ? "remove-btn" : "add-btn"}
-          onClick={() => (isSelected ? onRemoveDish(dish.id) : onAddDish(dish))}
-        >
-          {isSelected ? "Remove" : "Add +"}
-        </button>
-      </div>
-    </div>
+            <button
+              className="ingredient-link"
+              onClick={() => onViewIngredients(dish)}
+            >
+              <img
+                src="https://res.cloudinary.com/dehz5pshe/image/upload/v1757861320/fi_8508747_g3nj8v.svg"
+                alt="ingrediant icon"
+                className="icon"
+              />
+              Ingredient
+            </button>
+          </div>
+        </div>
+      )}
+    </Popup>
   );
 };
 
--- src/components/DishList.js
+++ src/components/DishList.js
@@ -1,4 +1,7 @@
 import "../App.css";
+
+
+
 import DishCard from "./DishCard";
 
 const DishList = ({
